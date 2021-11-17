bbfd1312dad945c5eeedfefdab93d083721358bb
JonasUJ <jonas@ulsnes.dk>
2021-11-17 18:36:57 +0100

    Spam 'as usize' everywhere (and imporve performance by a lot)

--- src/unionfind/mod.rs
+++ src/unionfind/mod.rs
@@ -3,14 +3,14 @@
 
 /// Represents a node in the [`UnionFind`].
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
-pub struct Site(pub usize);
+pub struct Site(pub u32);
 
 /// A [`Site`] container that also stores a representation of their connections.
 #[derive(Clone)]
 pub struct UnionFind {
-    count: usize,
+    count: u32,
     id: Vec<Site>,
-    sz: Vec<usize>,
+    sz: Vec<u32>,
 }
 
 impl UnionFind {
@@ -19,11 +19,11 @@ impl UnionFind {
     /// # Arguments
     ///
     /// * `count` - The number of initial component.
-    pub fn new(count: usize) -> Self {
+    pub fn new(count: u32) -> Self {
         UnionFind {
             count,
             id: (0..count).map(Site).collect(),
-            sz: vec![1; count],
+            sz: vec![1; count as usize],
         }
     }
 
@@ -44,7 +44,7 @@ impl UnionFind {
     /// assert_eq!(uf.find(a).unwrap(), b);
     /// ```
     pub fn find(&mut self, mut site: Site) -> Option<Site> {
-        if site.0 >= self.id.len() {
+        if site.0 as usize >= self.id.len() {
             return None;
         }
 
@@ -82,10 +82,10 @@ impl UnionFind {
     /// }
     /// ```
     pub unsafe fn find_unchecked(&mut self, mut site: Site) -> Site {
-        while site != *self.id.get_unchecked(site.0) {
-            std::intrinsics::assume(site.0 < self.id.len());
-            self.id[site.0] = *self.id.get_unchecked(self.id[site.0].0);
-            site = self.id[site.0];
+        while site != *self.id.get_unchecked(site.0 as usize) {
+            std::intrinsics::assume((site.0 as usize) < self.id.len());
+            self.id[site.0 as usize] = *self.id.get_unchecked(self.id[site.0 as usize].0 as usize);
+            site = self.id[site.0 as usize];
         }
 
         site
@@ -156,18 +156,18 @@ impl UnionFind {
         // SAFETY: Bounds checking done callers.
         unsafe {
             std::intrinsics::assume(
-                self.sz.len() > a.0
-                    && self.sz.len() > b.0
-                    && self.id.len() > a.0
-                    && self.id.len() > b.0,
+                self.sz.len() > a.0 as usize
+                    && self.sz.len() > b.0 as usize
+                    && self.id.len() > a.0 as usize
+                    && self.id.len() > b.0 as usize,
             );
         }
-        if self.sz[a.0] < self.sz[b.0] {
-            self.id[a.0] = b;
-            self.sz[b.0] += self.sz[a.0];
+        if self.sz[a.0 as usize] < self.sz[b.0 as usize] {
+            self.id[a.0 as usize] = b;
+            self.sz[b.0 as usize] += self.sz[a.0 as usize];
         } else {
-            self.id[b.0] = a;
-            self.sz[a.0] += self.sz[b.0];
+            self.id[b.0 as usize] = a;
+            self.sz[a.0 as usize] += self.sz[b.0 as usize];
         }
         self.count -= 1;
     }
@@ -210,7 +210,7 @@ impl UnionFind {
     /// uf.union(Site(2), Site(3));
     /// assert_eq!(uf.count(), 3);
     /// ```
-    pub fn count(&self) -> usize {
+    pub fn count(&self) -> u32 {
         self.count
     }
 }
@@ -220,7 +220,7 @@ macro_rules! bounds_checked_find {
     ($name:ident, $self:ident, $site:ident, $body:block) => {
         #[inline]
         pub fn $name(&mut $self, mut $site: Site) -> Option<Site> {
-            if $site.0 >= $self.id.len() {
+            if $site.0 as usize >= $self.id.len() {
                 return None;
             }
             $site = $body;
@@ -246,35 +246,35 @@ impl UnionFind {
 
     bounds_checked_find!(find_plain, self, site, {
         unsafe {
-            while site != *self.id.get_unchecked(site.0) {
-                site = *self.id.get_unchecked(site.0);
+            while site != *self.id.get_unchecked(site.0 as usize) {
+                site = *self.id.get_unchecked(site.0 as usize);
             }
         }
         site
     });
 
     bounds_checked_find!(find_plain_safe, self, site, {
-        while site != self.id[site.0] {
-            site = self.id[site.0];
+        while site != self.id[site.0 as usize] {
+            site = self.id[site.0 as usize];
         }
         site
     });
 
     bounds_checked_find!(find_compress_one, self, site, {
         unsafe {
-            while site != *self.id.get_unchecked(site.0) {
-                std::intrinsics::assume(site.0 < self.id.len());
-                self.id[site.0] = *self.id.get_unchecked(self.id[site.0].0);
-                site = self.id[site.0];
+            while site != *self.id.get_unchecked(site.0 as usize) {
+                std::intrinsics::assume((site.0 as usize) < self.id.len());
+                self.id[site.0 as usize] = *self.id.get_unchecked(self.id[site.0 as usize].0 as usize);
+                site = self.id[site.0 as usize];
             }
         }
         site
     });
 
     bounds_checked_find!(find_compress_one_safe, self, site, {
-        while site != self.id[site.0] {
-            self.id[site.0] = self.id[self.id[site.0].0];
-            site = self.id[site.0];
+        while site != self.id[site.0 as usize] {
+            self.id[site.0 as usize] = self.id[self.id[site.0 as usize].0 as usize];
+            site = self.id[site.0 as usize];
         }
         site
     });
@@ -282,10 +282,10 @@ impl UnionFind {
     bounds_checked_find!(find_compress_one_do_while, self, site, {
         unsafe {
             while {
-                std::intrinsics::assume(site.0 < self.id.len());
-                self.id[site.0] = *self.id.get_unchecked(self.id[site.0].0);
-                site = self.id[site.0];
-                site != *self.id.get_unchecked(site.0)
+                std::intrinsics::assume((site.0 as usize) < self.id.len());
+                self.id[site.0 as usize] = *self.id.get_unchecked(self.id[site.0 as usize].0 as usize);
+                site = self.id[site.0 as usize];
+                site != *self.id.get_unchecked(site.0 as usize)
             } {}
         }
 
@@ -294,9 +294,9 @@ impl UnionFind {
 
     bounds_checked_find!(find_compress_one_do_while_safe, self, site, {
         while {
-            self.id[site.0] = self.id[self.id[site.0].0];
-            site = self.id[site.0];
-            site != self.id[site.0]
+            self.id[site.0 as usize] = self.id[self.id[site.0 as usize].0 as usize];
+            site = self.id[site.0 as usize];
+            site != self.id[site.0 as usize]
         } {}
 
         site
@@ -304,19 +304,19 @@ impl UnionFind {
 
     bounds_checked_find!(find_compress, self, site, {
         let mut seen: [Site; 8] = [Site(0); 8];
-        let mut i: usize = 0;
+        let mut i = 0;
         unsafe {
-            while site != self.id[site.0] {
-                seen[i & 7] = self.id[site.0];
-                site = self.id[site.0];
-                std::intrinsics::assume(site.0 < self.id.len());
+            while site != self.id[site.0 as usize] {
+                seen[(i & 7) as usize] = self.id[site.0 as usize];
+                site = self.id[site.0 as usize];
+                std::intrinsics::assume((site.0 as usize) < self.id.len());
                 i += 1;
             }
             i = std::cmp::min(i, seen.len());
             for j in 0..i {
                 let child = *seen.get_unchecked(j);
-                std::intrinsics::assume(child.0 < self.id.len());
-                self.id[child.0] = site;
+                std::intrinsics::assume((child.0 as usize) < self.id.len());
+                self.id[child.0 as usize] = site;
             }
         }
         site
@@ -324,15 +324,15 @@ impl UnionFind {
 
     bounds_checked_find!(find_compress_safe, self, site, {
         let mut seen: [Site; 8] = [Site(0); 8];
-        let mut i: usize = 0;
-        while site != self.id[site.0] {
-            seen[i & 7] = self.id[site.0];
-            site = self.id[site.0];
+        let mut i = 0;
+        while site != self.id[site.0 as usize] {
+            seen[(i & 7) as usize] = self.id[site.0 as usize];
+            site = self.id[site.0 as usize];
             i += 1;
         }
         i = std::cmp::min(i, seen.len());
         for child in seen.iter().take(i) {
-            self.id[child.0] = site;
+            self.id[child.0 as usize] = site;
         }
         site
     });
@@ -344,14 +344,14 @@ pub fn read_test_file(filename: &str) -> (UnionFind, Vec<(Site, Site)>) {
     use std::fs;
     let string_content = fs::read_to_string(filename).unwrap();
     let mut content = string_content.trim().split('\n');
-    let count = content.next().unwrap().parse::<usize>().unwrap();
+    let count = content.next().unwrap().parse::<u32>().unwrap();
     (
         UnionFind::new(count),
         content
             .map(|l| {
                 l.split_ascii_whitespace()
-                    .map(|n| n.parse::<usize>().unwrap())
-                    .collect::<Vec<usize>>()
+                    .map(|n| n.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>()
             })
             .map(|p| (Site(p[0]), Site(p[1])))
             .collect(),
